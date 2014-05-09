687660f366764f809ee2d1f4383d3aaccd978b70
abhiShandy <abhishek.sandy@gmail.com>
2014-05-09 14:15:55 +0530

    play/pause button

--- javascripts/logo_border.js
+++ javascripts/logo_border.js
@@ -4,11 +4,24 @@ var c = document.getElementById('cube');
 t.style.WebkitAnimationPlayState="paused";
 c.style.WebkitAnimationPlayState="paused";		
 img.style.WebkitAnimationPlayState="paused";		
-var trial = function(s){
+var p = false;
+var play = function(){
 	c.style.WebkitAnimationPlayState="running";
 	t.style.WebkitAnimationPlayState="running";
 	img.style.WebkitAnimationPlayState="running";				
+	p=true;
 }
-var trigger = function(){
-	trial("play");		
+var pause = function(){
+	c.style.WebkitAnimationPlayState="paused";
+	t.style.WebkitAnimationPlayState="paused";
+	img.style.WebkitAnimationPlayState="paused";	
+	p=false;
+}
+var toggleAnimation = function(){
+	if(p){
+		pause();
+	}
+	else{
+		play();
+	}
 }
